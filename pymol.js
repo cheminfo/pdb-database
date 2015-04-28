ea54a9e1cbeb9edb72eb2b596947641800f1d0df
stropitek <kostro.d@gmail.com>
2015-04-28 14:08:03 +0200

    pymol: height and width as options

--- pymol.js
+++ pymol.js
@@ -1,7 +1,15 @@
 var fs = require('fs');
 var gm = require('gm');
+var _ = require('lodash');
 
-exports = module.exports = function(id, pdb) {
+exports = module.exports = function(id, pdb, options) {
+
+    var defaultOptions = {
+        width: 200,
+        height: 200
+    };
+
+    options = _.defaults(options, defaultOptions);
     return new Promise(function(resolve, reject) {
         var pdbFile = id+'.pdb';
         var pngFile = id+'.png';
@@ -10,25 +18,23 @@ exports = module.exports = function(id, pdb) {
                 reject('could not write file');
             }
 
-            var exec = require('child_process').exec,child;
+            var exec = require('child_process').exec;
             var cmd = 'pymol -c ' + pdbFile + ' -d "as ribbon;spectrum count;set seq_view" -g ' + pngFile;
-            child = exec(cmd,
-                    function (error, stdout, stderr) {
+            exec(cmd,
+                function (error) {
 
-                        fs.unlinkSync(pdbFile);
-                        if (error !== null) {
-                            console.log('exec error: ' + error);
-                            return reject(error);
-                        }
-                        gm(pngFile).resize(200,200).toBuffer('gif', function(err,buffer){
-                            fs.unlinkSync(pngFile);
-                            if(err) return reject(err);
+                    fs.unlinkSync(pdbFile);
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                        return reject(error);
+                    }
+                    gm(pngFile).resize(options.width, options.height).toBuffer('gif', function(err,buffer){
+                        fs.unlinkSync(pngFile);
+                        if(err) return reject(err);
 
-                            return resolve(buffer);
-                        });
-
-
+                        return resolve(buffer);
                     });
+                });
         });
     });
 };
