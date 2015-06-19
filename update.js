922218e996b5beafbda57fbea65538dc51d91dcf
Daniel Kostro <kostro.d@gmail.com>
2015-06-19 10:39:12 +0200

    rewrite update script

--- update.js
+++ update.js
@@ -3,11 +3,7 @@
 
 var config = require('./config.js')();
 var Rsync = require ('rsync');
-var async = require ('async');
 var common = require('./common');
-
-var pdb = common.createCouchDB(config.couch);
-
 var newFiles=[];
 var rsync = new Rsync();
 rsync.source(config.rsync.source);
@@ -26,27 +22,19 @@ rsync.output(
 );
 
 rsync.execute(function(error, code, cmd) {
+    console.log('rysinc executed, now building database');
     if (error) {
         console.log("RSYNC ERROR");
         console.log(error);
         console.log(code);
         console.log(cmd);
-
-        processPdbs(newFiles, callback);
-    } else {
-        processPdbs(newFiles, callback);
     }
+    Promise.resolve()
+        .then(common.processPdbs(files))
+        .then(common.processPdbAssemblies(files))
+        .catch(errorHandler);
 });
 
-// this file is gzip, we need to uncompress it
-function processPdbs(newFiles, callback) {
-    if (newFiles && newFiles.length>0) {
-        async.mapSeries(newFiles, common.processPdb, function(err) {
-            if(err) console.error(err);
-            callback(null, newFiles);
-        })
-    }
-}
 
 function errorHandler(err) {
     console.log('An error occured', err, err.stack);
