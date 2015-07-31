469df1e6517cd837cb0bea0383b9fca2b81b01de
Daniel Kostro <kostro.d@gmail.com>
2015-07-31 16:07:14 +0200

    Add history of rsync change in files

--- update.js
+++ update.js
@@ -5,6 +5,7 @@ var config = require('./config.js')();
 var Rsync = require ('rsync');
 var common = require('./common');
 var fs = require('fs');
+var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
 
 var help = [
@@ -24,14 +25,25 @@ if(argv['pdb-asym-unit']) {
 
 if(argv['pdb-bio-assembly']) {
   console.log('Updating biological assemblies...');
-  prom = prom.then(doRsync(config.bioAssembly.rsync.source, config.bioAssembly.rsync.destination, common.processPdbAssemblies));
+  prom = prom.then(doRsync(config.bioAssembly.rsync.source, config.bioAssembly.rsync.destination, common.processPdbAssemblies, function(changed) {
+    console.log('Writing rsync change');
+    var dir = config.bioAssembly.rsync.historyDir;
+    if(!dir) return;
+    dir = path.join(dir, '' + Date.now() + '.json');
+    fs.writeFileSync(dir, JSON.stringify(changed));
+  }));
 }
 
 prom.catch(errorHandler);
 
-function doRsync(source, destination, fn) {
+function doRsync(source, destination, fn, changedCallback) {
   return function() {
     return new Promise(function(resolve, reject) {
+      var changed = {
+        deleted: [],
+        update: []
+      };
+
       var newFiles=[];
       var rsync = new Rsync();
       rsync.source(source);
@@ -43,10 +55,14 @@ function doRsync(source, destination, fn) {
         function(data){
           // do things like parse progress
           var line=data.toString().replace(/[\r\n].*/g,"");
-          if(line.startsWith('deleting ')) return;
+          if(line.startsWith('deleting ')) {
+            changed.deleted.push(line);
+            return;
+          }
           if (line.match(/\.gz$/)){
             fs.appendFileSync('./rsyncChanges', config.asymetrical.rsync.destination + line + '\n');
             newFiles.push(config.asymetrical.rsync.destination + line);
+            changed.update.push(line);
           }
         }, function(data) {
           // do things like parse error output
@@ -54,6 +70,9 @@ function doRsync(source, destination, fn) {
       );
 
       rsync.execute(function(error, code, cmd) {
+        if(changedCallback) {
+          changedCallback(changed);
+        }
         console.log('rysnc executed, now building database');
         if (error) {
           console.log("RSYNC ERROR, did not build database");
